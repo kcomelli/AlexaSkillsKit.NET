da3dc204d928093cd5b25c7ae0b197dd26109d63
Tony Surma <tony@surmafamily.com>
2017-03-22 22:07:14 -0500

    add ASK sample files and WebApiCompatShim

---
+++ AlexaSkillsKit.SampleCore/Speechlet/AlexaController.cs
@@ -0,0 +1,23 @@
+﻿//  Copyright 2015 Stefan Negritoiu (FreeBusy). See LICENSE file for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using AlexaSkillsKit;
+using System.Web.Http;
+
+namespace Sample.Controllers
+{
+    [Route("alexa/sample-session")]
+    public class AlexaController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage SampleSession() {
+            var speechlet = new SampleSessionSpeechlet();
+            return speechlet.GetResponse(Request);
+        }
+    }
+}
